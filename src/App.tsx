13b824450eb5636077ca8a2cfc24e02aa5981656
Raktim Mondol <raktim.live@gmail.com>
2024-11-19 13:36:18 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,25 @@ import Research from './components/Research';
 import Publications from './components/Publications';
 import Awards from './components/Awards';
 import Contact from './components/Contact';
+import { AudioProvider } from './components/AudioContext';
 
 function App() {
   return (
-    <div className="min-h-screen bg-white">
-      <Navbar />
-      <div className="pt-16">
-        <Hero />
-        <Education />
-        <Experience />
-        <Skills />
-        <Research />
-        <Publications />
-        <Awards />
-        <Contact />
+    <AudioProvider>
+      <div className="min-h-screen bg-white">
+        <Navbar />
+        <div className="pt-16">
+          <Hero />
+          <Education />
+          <Experience />
+          <Skills />
+          <Research />
+          <Publications />
+          <Awards />
+          <Contact />
+        </div>
       </div>
-    </div>
+    </AudioProvider>
   );
 }
 
