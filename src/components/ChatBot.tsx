eb34d4ffb47f22bbd45b429b9485d516eb252113
Raktim Mondol <raktim.live@gmail.com>
2024-11-20 12:34:22 +1100

    Create ChatBot.tsx

---
+++ src/components/ChatBot.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'zapier-interfaces-chatbot-embed': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        'is-popup'?: string;
+        'chatbot-id': string;
+      };
+    }
+  }
+}
+
+export default function ChatBot() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js';
+    script.type = 'module';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <zapier-interfaces-chatbot-embed 
+      is-popup="true" 
+      chatbot-id="cm3p5x7m7006l5z3l37aldbhi"
+    />
+  );
+}
